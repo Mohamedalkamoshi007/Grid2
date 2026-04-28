f1d8f584e645434160ff53cc7c30428da606aa4d
Mohamedalkamoshi007 <mohammedak841@gmail.com>
2026-04-28 06:09:53 +0200

    add function(comper betwen two state (samestate) ,visited, blocks, updatecoins, refill fuel ,manhatten)

--- Grid2.cpp
+++ Grid2.cpp
@@ -12,17 +12,16 @@ public:
     int x;
     int y;
     int fuel;
-
     char c1,c2,c3,c4;
 
     void print()
     {
-        cout<<"("<< x << ","<< y << ","<< fuel << ","<< c1 << ","<< c2 << ","<< c3 << ","<< c4 << ")";
+        cout<<"("<<x<<","<<y<<","<<fuel<<","<<c1<<","<<c2<<","<<c3<<","<<c4<<")";
     }
 
-    bool isGoal()
+    bool Goalcheck()
     {
-        if (x == 1 &&y == 1 &&c1 == 't' &&c2 == 't' &&c3 == 't' &&c4 == 't')
+        if(x==1 && y==1 && c1=='t' && c2=='t' && c3=='t' && c4=='t')
         {
             return true;
         }
@@ -35,9 +34,7 @@ class Node
 {
 public:
     State state;
-
     int parent;
-
     int g,h,f;
 };
 
@@ -45,11 +42,10 @@ class GridGame
 {
 private:
     Node nodes[MAX_STATES];
-
     int nodeCount;
 
-    int coinX[4] = {1, 2, 5, 6};
-    int coinY[4] = {2, 5, 5, 9};
+    int coinX[4]={1,2,5,6};
+    int coinY[4]={2,5,5,9};
 
     int fuelX;
     int fuelY;
@@ -57,15 +53,14 @@ private:
 public:
     GridGame()
     {
-        nodeCount = 0;
-
-        fuelX = 6;
-        fuelY = 6;
+        nodeCount=0;
+        fuelX=6;
+        fuelY=6;
     }
 
     int absValue(int n)
     {
-        if (n<0)
+        if(n<0)
         {
             return -n;
         }
@@ -73,12 +68,60 @@ public:
         return n;
     }
 
-    int maxValue(int a, int b)
+    int maxValue(int a,int b)
     {
-        if (a>b)
+        if(a>b)
         {
             return a;
         }
 
         return b;
     }
+bool sameState(State a,State b){
+    if(a.x==b.x && a.y==b.y && a.fuel==b.fuel && a.c1==b.c1 && a.c2==b.c2 && a.c3==b.c3 && a.c4==b.c4){
+        return true;
+    }
+    return false;
+
+}
+bool visited(State s){
+    for(int i=0;i<nodeCount;i++){
+        if(sameState(nodes[i].state,s)){
+            return true;
+
+        }
+
+    }
+    return false;
+
+}
+bool BlockedCheck(int x,int y){
+    if((x==2 && y==2) || (x==3 && y==3) || (x==4 && y==4)){
+        return true;
+
+    }
+    return false;
+
+}
+void updateCoins(State &s){
+if(s.x==coinX[0] && s.y==coinY[0]){
+    s.c1='t';
+}
+if(s.x==coinX[1] && s.y==coinY[1]){
+    s.c2='t';
+}
+if(s.x==coinX[2] && s.y==coinY[2]){
+    s.c3='t';
+}
+if(s.x==coinX[3] && s.y==coinY[3]){
+    s.c4='t';
+}
+}
+void refillFuel(State &s){
+if(s.x==fuelX && s.y==fuelY){
+s.fuel=20;
+}
+}
+int manhatten(int x1,int y1,int x2,int y2){
+return absValue(x1 - x2) + absValue(y1 - y2);
+}
